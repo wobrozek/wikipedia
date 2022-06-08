7f002169b3ed8f3303b9f634884d2aaef61385cb
Wojciech Brożek <wobrozek@gmail.com>
2022-06-08 22:50:50 +0200

    add search and urls config

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.wiki, name="wiki"),
+    path("wiki", views.wikiForm, name="wikiForm")
 ]
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from . import util
 
+class newForm(forms.Form):
+    search=forms.CharField(label='Szukaj tematu ', max_length=50)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form":newForm()
     })
+
+def wiki(request , title):
+    if(util.get_entry(title))!=None:
+        return render(request, "encyclopedia/wiki.html", {
+            "title": title,
+            "form": newForm()
+                      })
+    else:
+        return render(request, "encyclopedia/wiki.html", {
+            "title": "Page not found",
+            "form":newForm()
+    })
+
+def wikiForm(request):
+    if request.method == "POST":
+        form = newForm(request.POST)
+        if form.is_valid():
+            dane=form.cleaned_data['search'];
+            if(util.get_entry(dane)!=None):
+
+                return render(request, "encyclopedia/wiki.html", {
+                    "title": dane,
+                    "form": newForm()
+                })
+
+    return render(request, "encyclopedia/wiki.html", {
+            "title": "Page not found",
+            "form":newForm()
+    })
+
 

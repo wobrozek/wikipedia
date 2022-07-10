630809356dd5bafac268985cb323577e4a638bff
Wojciech Brożek <wobrozek@gmail.com>
2022-07-10 13:16:39 +0200

    add random and comments

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
-    path("wiki", views.wikiForm, name="wikiForm")
+    path("wiki", views.wikiForm, name="wikiForm"),
+    path("randomWiki", views.randomWiki, name="randomWiki")
 ]
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from django import forms
 from django.http import HttpResponseRedirect
@@ -8,12 +10,18 @@ class newForm(forms.Form):
     search=forms.CharField(label='Szukaj tematu ', max_length=50)
 
 def index(request):
+    '''
+    show all websites (entries)
+    '''
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
         "form":newForm()
     })
 
 def wiki(request , title):
+    '''
+        if page entries exist show it else display error page
+    '''
     if(util.get_entry(title))!=None:
         return render(request, "encyclopedia/wiki.html", {
             "title": title,
@@ -25,13 +33,24 @@ def wiki(request , title):
             "form":newForm()
     })
 
+def randomWiki(request):
+    '''
+    function that select random page from a list of entries
+    '''
+    list=util.list_entries()
+    wikiIndex=random.randint(0,len(list)-1)
+    return HttpResponseRedirect(f"wiki/{list[wikiIndex]}")
+
+
 def wikiForm(request):
+    '''
+    function that gives form data to function wiki
+    '''
     if request.method == "POST":
         form = newForm(request.POST)
         if form.is_valid():
             dane=form.cleaned_data['search'];
-            if(util.get_entry(dane)!=None):
-                return HttpResponseRedirect(f"wiki/{dane}")
+            return HttpResponseRedirect(f"wiki/{dane}")
 
     return HttpResponseRedirect(f"wiki/error")
 
